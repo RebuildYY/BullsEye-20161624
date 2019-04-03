6fa6944fa3df7f8acb0a40199f2fe360c444f14e
LeeYunSeok <dbstjrrl803@naver.com>
2019-04-03 16:26:30 +0900

    ver2.2

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 50
+    var targetValue = 0
+    
     @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,17 +26,34 @@ class ViewController: UIViewController {
     
     
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "O.K", style: .default, handler: nil)
+        
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
     }
     
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
         print(" The value of the slider is now:\(currentValue)")
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in:1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
     
 }
