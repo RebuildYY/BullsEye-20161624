37cf85d8fce36b4deab51b968cee46160a38faf1
LeeYunSeok <dbstjrrl803@naver.com>
2019-04-03 16:41:46 +0900

    ver3

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     var currentValue: Int = 50
     var targetValue = 0
+    var score = 0
+
     
     @IBOutlet weak var slider: UISlider!
     
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +29,10 @@ class ViewController: UIViewController {
     
     
     @IBAction func showAlert(_ sender: Any) {
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
         let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
@@ -54,6 +61,7 @@ class ViewController: UIViewController {
     }
     func updateLabels(){
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
     }
     
 }
